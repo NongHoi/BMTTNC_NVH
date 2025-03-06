f55428f6145256af1bdeb61b9a0629f7cedd83f7
NongHoi <123308415+NongHoi@users.noreply.github.com>
2025-03-06 14:27:37 +0700

    Delete BaiTap2 directory

--- BaiTap2/c2.py
+++
@@ -1,6 +0,0 @@
-def dao_list(lst): 
-    return lst[::-1]
-input_list = input("Nhap ds cac so , cach bang dau phay: ")
-numbers = list(map(int , input_list.split(',')))
-list_dao = dao_list(numbers)
-print("List da dao dc: ", list_dao)
